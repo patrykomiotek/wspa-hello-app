4b8d8fb463f730871ea893a196a3f03a1ad3ae1b
Patryk Omiotek <patryk@macbook.local>
2023-05-08 19:55:30 +0200

    initial commit

---
+++ date.js
@@ -0,0 +1,4 @@
+import { format } from 'date-fns';
+
+console.log(new Date());
+console.log(format(new Date(), "'Today is a' eeee"));
---
+++ hello.js
@@ -0,0 +1,12 @@
+// console.log('Hello');
+
+const PI = 3.14;
+// PI = 5;
+
+let a = 15;
+a = 16;
+a = 'Hello there!';
+
+// console.log(a);
+const output = `${a} PI value is ${PI}`;
+console.log(output);
